561e58733947861b1d1b9284583cdeacac4c3dd5
Gerry CR <gerry.dev73@gmail.com>
2024-12-23 01:06:21 -0600

    agregamos ultima aclaracion sobre las rutas en web.php

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', function () {
     // return view('welcome');
     return view('task.index');
 });
+
+// ojo, la ruta no debe auntar a la funcion, sino al controlador ya definido en app/Http
+
+// podemos acceder a las rutas a travez del archvo web.php para manipular todas las rutas que el usuario solicita
